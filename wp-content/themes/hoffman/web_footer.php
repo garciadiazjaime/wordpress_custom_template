7da5dad1df708ea89082674766d8a0885dab4fa1
Jaime Garcia <jgarciaqo@gmail.com>
2015-11-24 20:49:50 -0600

    customize

---
+++ wp-content/themes/hoffman/web_footer.php
@@ -0,0 +1,79 @@
+<footer data-ui-view="footer-view" class="ng-scope">
+<div id="footer-head" class="ng-scope">
+	<div class="container text-right">
+		<p><span class="hidden">Vega</span> <span data-translate="string.c" class="ng-scope">abogados</span></p>
+	</div>
+</div>
+<div id="footer-main" class="ng-scope">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-4">
+				<div class="row">
+					<div class="col-xs-12 col-sm-6">
+						<ul>
+							<li><h5><a href="/nosotros" data-ui-sref="app.nosotros" title="About us" data-translate="menu.whoweare" class="ng-scope">¿Quiénes Somos?</a></h5></li>
+							<li><h5><a href="/nosotros/nuestro-equipo" data-ui-sref="app.nosotros.nuestroequipo" title="Our team" data-translate="menu.team" class="ng-scope">Nuestro Equipo</a></h5></li>
+						</ul>
+					</div>
+					<div class="col-xs-12 col-sm-6">
+						<ul>
+							<li><h5><a href="/que-hacemos" data-ui-sref="app.quehacemos" title="What we do" data-translate="menu.whatwedo" class="ng-scope">¿Qué hacemos?</a></h5></li>
+							<li><h5><a href="/que-hacemos/campo-accion" data-ui-sref="app.quehacemos.campoaccion" title="Our practice" data-translate="menu.practice" class="ng-scope">Áreas de Práctica</a></h5></li>
+						</ul>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-12 col-sm-12">
+						<div class="brochure ng-scope" data-translate="docs.brochure"><a href="/static/docs/VGA_DocumentoPerfil-Esp-01.pdf" target="_blank" class="custom_submit"><span>Descargar Brochure Corporativo</span><i></i></a></div>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-8">
+				<h5><a href="/contacto" data-ui-sref="app.contacto" title="Contact" data-translate="menu.contact" class="ng-scope">Contacto</a></h5>
+				<div class="row footer-text">
+					<div class="col-xs-12 col-sm-4">
+						<h6>Tijuana</h6>
+						<p data-translate="address.tijuana" class="ng-scope">Edificio CIMA<br>José Gorostiza 1165-8 Zona Río<br>Tijuana, B. C. 22010</p>
+						<a href="tel:011526646835759" title="Call Us" class="tel">011-52-664-683-5759</a>
+					</div>
+					<div class="col-xs-12 col-sm-4">
+						<h6>Mexicali</h6>
+						<p data-translate="address.mexicaly" class="ng-scope">Avenida Madero #1250<br>Colonia Nueva, Segunda Sección<br>Mexicali B.C. 21100</p>
+						<a href="tel:011526865526009" title="Call Us" class="tel">01152-686-5526009 <span data-translate="contact.and" class="ng-scope">y</span> 10</a>
+					</div>
+					<div class="col-xs-12 col-sm-4">
+						<h6>Chula Vista</h6>
+						<p data-translate="address.chulavista" class="ng-scope">819 Anchorage Place Suite 108<br>Chula Vista CA. 91914</p>
+						<a href="tel:16193709387" title="Call Us" class="tel">1-619-370-9387</a>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-12 col-sm-8">
+						<a href="mailto:info@vega.sc" title="Write Us" class="email">info@vega.sc</a>
+					</div>
+					<div class="col-xs-12 col-sm-4">
+			            <ul class="social">
+		                <li class="facebook"><a href="https://www.facebook.com/vegalawconsulting?fref=ts" title="Facebook" target="_blank">Facebook</a></li>
+		                <li class="linkedin"><a href="https://www.linkedin.com/company/vega-abogados" title="LinkedIn" target="_blank">LinkedIn</a></li>
+		                <li class="twitter"><a href="https://twitter.com/vega_abogadossc" title="Twitter" target="_blank">Twitter</a></li>
+			        	</ul>
+					</div>
+				</div>
+			</div>
+		</div><!-- .row -->
+	</div>
+</div>
+<div id="footer-bottom" class="ng-scope">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-4">
+				<p>VEGA Abogados, All rights reserved 2015</p>
+			</div>
+			<div class="col-xs-12 col-sm-4 text-center">
+			</div>
+			<div class="col-xs-12 col-sm-4 text-right">
+				<p id="credit">A project made by: <a href="http://somospool.com" title="Branding POOL" target="_blank">Pool</a> Coded by: <a href="http://www.mintitmedia.com" title="Mint IT Media - Diseño y desarrollo web" target="_blank">Mint</a></p>
+			</div>
+		</div>
+	</div>
+</div></footer>
---
+++ wp-content/themes/hoffman/web_header.php
@@ -0,0 +1,37 @@
+<header id="header" data-ui-view="header-view" class="ng-scope"><nav class="navbar navbar-default ng-scope" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-ng-init="navCollapsed = true" data-ng-click="navCollapsed = !navCollapsed">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/inicio" data-ui-sref="app.inicio">Brand</a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" data-ng-class="!navCollapsed &amp;&amp; 'in'" data-ng-click="navCollapsed=true">
+            <ul class="nav navbar-nav">
+				<li><a href="/inicio" data-ui-sref="app.inicio" data-ui-sref-active="active" data-translate="menu.home" class="ng-scope active">Inicio</a></li>
+				<li><a href="/nosotros" data-ui-sref="app.nosotros" data-ui-sref-active="active" data-translate="menu.aboutus" class="ng-scope">¿Quiénes Somos?</a></li>
+				<li><a href="/que-hacemos" data-ui-sref="app.quehacemos" data-ui-sref-active="active" data-translate="menu.whatwedo" class="ng-scope">¿Qué hacemos?</a></li>
+                <li><a href="/contacto" data-ui-sref="app.contacto" data-ui-sref-active="active" data-translate="menu.contact" class="ng-scope">Contacto</a></li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+        <div id="header-tools">
+            <ul class="social">
+                <li class="facebook">
+                    <a href="https://www.facebook.com/vegalawconsulting?fref=ts" title="Facebook" target="_blank">Facebook</a>
+                </li><li class="linkedin">
+                    <a href="https://www.linkedin.com/company/vega-abogados" title="Linkedin" target="_blank">Linkedin</a>
+                </li><li class="twitter">
+                    <a href="https://twitter.com/vega_abogadossc" title="Twitter" target="_blank">Twitter</a>
+                </li>
+        	</ul><!-- .social -->
+            <p id="lang" class="btn-group hide">
+                <a href="" title="Change to english" data-ng-click="switchLang('en_EN')" id="lang_en_EN">Eng</a> -
+                <a href="" title="Change to english" data-ng-click="switchLang('es_MX')" id="lang_es_MX" class="active">Esp</a>
+            </p><!-- #lang -->
+        </div>
+    </div>
+</nav></header>
