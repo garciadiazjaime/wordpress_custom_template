401c05b29d0ab35a4bd173c05c4e6021e78cb5d1
Jaime Garcia <jgarciaqo@gmail.com>
2015-11-26 11:50:45 -0600

    set data dir

--- wp-config.php
+++ wp-config.php
@@ -38,7 +38,8 @@ define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 
 if(getenv('OPENSHIFT_DATA_DIR')){
-	define( 'UPLOADS', getenv('OPENSHIFT_DATA_DIR') + 'wp-content/uploads' );
+	echo "=========HERE=========";
+	define('UPLOADS', getenv('OPENSHIFT_DATA_DIR'));
 }
 
 /**#@+
